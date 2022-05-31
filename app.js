7eff7e23266c43a651f5001d07be700a7bf38479
CristoforosChoras <christopher.choras@yahoo.com>
2022-05-31 15:50:36 +0300

    some basic styles

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const passwordButton = document.querySelector("label");
 const div = document.querySelector(".mainDiv");
 const body = document.querySelector("body");
+const button = document.querySelector("button")
 
 passwordButton.style.color = "white";
+passwordButton.style.fontSize = "20px";
+button.style.width= "50%"
+button.style.height= "20%"
+button.style.borderRadius= "5%"
+
+
 div.style.width = "500px";
-div.style.height = "80px";
+div.style.height = "200px";
 div.style.backgroundColor = "black";
 div.style.display = "flex";
 div.style.flexDirection = "column";
 div.style.justifyContent = "center";
 div.style.alignItems = "center";
+div.style.borderRadius= "100%"
 
 function generatePassword() {
   let length = 8,
