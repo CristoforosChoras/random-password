bfdfd2d19346d3f8bd35fd1a5545938244e57cc5
CristoforosChoras <christopher.choras@yahoo.com>
2022-05-31 16:55:17 +0300

    more

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const passwordButton = document.querySelector("label");
+const password = document.querySelector("label");
 const div = document.querySelector(".mainDiv");
 const body = document.querySelector("body");
-const button = document.querySelector("button")
+const button = document.querySelector("button");
 
-passwordButton.style.color = "white";
-passwordButton.style.fontSize = "20px";
-button.style.width= "50%"
-button.style.height= "20%"
-button.style.borderRadius= "5%"
 
+password.style.color = "white";
+password.style.fontSize = "20px";
+button.style.width = "50%";
+button.style.height = "20%";
+button.style.borderRadius = "5%";
 
 div.style.width = "500px";
 div.style.height = "200px";
@@ -17,7 +17,7 @@ div.style.display = "flex";
 div.style.flexDirection = "column";
 div.style.justifyContent = "center";
 div.style.alignItems = "center";
-div.style.borderRadius= "100%"
+div.style.borderRadius = "100%";
 
 function generatePassword() {
   let length = 8,
@@ -25,9 +25,11 @@ function generatePassword() {
       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!§$%&/(",
     passwordGenerated = "";
   for (let i = 0, n = charset.length; i < length; ++i) {
-    passwordButton.innerHTML = passwordGenerated += charset.charAt(
+    password.innerHTML = passwordGenerated += charset.charAt(
       Math.floor(Math.random() * n)
     );
   }
+
   return passwordGenerated;
 }
+
