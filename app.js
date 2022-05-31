839cbcc64e58fd6e9a968b7b2136df9b3f479fcb
CristoforosChoras <christopher.choras@yahoo.com>
2022-05-31 15:31:41 +0300

    creating a div

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const passwordButton = document.querySelector("label")
+const passwordButton = document.querySelector("label");
+const div = document.querySelector(".mainDiv")
 
 
 
+div.style.width="100px"
+div.style.height="100px"
+div.style.backgroundColor = "black"
 
 
 
 
 function generatePassword() {
-    let length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!§$%&/(",
-        passwordGenerated = "";
-    for (let i = 0, n = charset.length; i < length; ++i) {
-        passwordButton.innerHTML=passwordGenerated += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return passwordGenerated;
+  let length = 8,
+    charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!§$%&/(",
+    passwordGenerated = "";
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    passwordButton.innerHTML = passwordGenerated += charset.charAt(
+      Math.floor(Math.random() * n)
+    );
+  }
+  return passwordGenerated;
 }
-
